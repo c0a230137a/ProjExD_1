ae80805f9efb8a010ca2b442fb196dd972e50747
C0A23013 <c0a230137a@edu.teu.ac.jp>
2024-11-19 16:32:07 +0900

    こうかとん移動

--- pygame_image.py
+++ pygame_image.py
@@ -14,12 +14,22 @@ def main():
     kk_img = pg.image.load("fig/3.png")  # こうかとん画像
     kk_img = pg.transform.flip(kk_img, True, False)  # 左右反転
     kk_rct = kk_img.get_rect()  # Rectで管理
-    kk_rct.topleft = (300, 200)  # 横300, 縦200の位置に配置 
+    kk_rct.center = 300, 200 # 横300, 縦200の位置に配置 
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        
+        key_lst = pg.key.get_pressed()
+        #print(key_lst[pg.K_UP],key_lst[pg.K_DOWN],key_lst[pg.K_LEFT],key_lst[pg.K_RIGHT])
+        if key_lst[pg.K_UP]:
+            kk_rct.move_ip(0 ,-1)
+        if key_lst[pg.K_DOWN]:
+            kk_rct.move_ip(0 ,1)
+        if key_lst[pg.K_LEFT]:
+            kk_rct.move_ip(-1 ,0)
+        if key_lst[pg.K_UP]:
+            kk_rct.move_ip(1 ,0)
+
         x = -(tmr%3200)
         
 
@@ -27,7 +37,7 @@ def main():
         screen.blit(bg_img2, [x+1600, 0])
         screen.blit(bg_img, [x+3200, 0]) #screeSufeces
         screen.blit(bg_img2, [x+4800, 0])
-        screen.blit(kk_img, [300, 200])
+        screen.blit(kk_img, kk_rct)
         pg.display.update()
         tmr += 1        
         clock.tick(200)
